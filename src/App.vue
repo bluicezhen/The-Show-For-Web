daf074b63cd2afa196acfa9fbead28fb4cb850ee
Bluice Zhen <bluice.zhen@gmail.com>
2016-11-26 22:52:48 +0800

    feat: basic router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-
+    <div id="app">
+        <router-view></router-view>
+    </div>
 </template>
 
-<script>
-export default {
-    name: 'app',
-    data () {return {}}}
+<script type="javascript">
+    export default {
+        name: 'app', data () {
+            return {}
+        }
+    };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router';
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-import App from './App.vue'
+import Vue from "vue"
+import VueRouter from "vue-router";
+import ElementUI from "element-ui"
+import "element-ui/lib/theme-default/index.css"
+import App from "./App.vue"
+
+import page_index from "./components/index.vue"
+import page_login from "./components/login.vue"
 
 Vue.use(ElementUI);
 Vue.use(VueRouter);
 
-const router = new VueRouter({routes: [
-
-]});
+const router = new VueRouter({
+    routes: [
+        {path: "/", component: page_index},
+        {path: "/login", component: page_login}
+    ]
+});
 
 new Vue({
-    el: '#app',
+    el: "#app",
     router: router,
     render: h => h(App)
 });
