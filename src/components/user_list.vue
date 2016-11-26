7ec523e8d046041e8a77f0c69ba89201fa9974e5
Bluice Zhen <bluice.zhen@gmail.com>
2016-11-26 15:39:49 +0800

    vue init

---
+++ src/components/user_list.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>用户列表</h1>
+        <hr>
+
+        <!-- 用户列表 Begin -->
+
+        <!-- 用户列表 End -->
+    </div>
+</template>
+
+<script type="text/javascript">
+
+    export default{
+    }
+</script>
